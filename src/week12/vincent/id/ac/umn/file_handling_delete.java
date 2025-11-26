d3760dce439995194bf87d462f912a799d5be7b1
VincentVergioUMN <vincent.vergio@student.umn.ac.id>
2025-11-26 11:05:55 +0700

    Delete myfile.txt

---
+++ src/week12/vincent/id/ac/umn/file_handling_delete.java
@@ -0,0 +1,18 @@
+package week12.vincent.id.ac.umn;
+
+import java.io.File;
+
+public class file_handling_delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File Obj = new File("myfile.txt");
+		if (Obj.delete()) {
+			System.out.println("The deleted file is : " + Obj.getName());
+		}
+		else {
+			System.out.println("Failed in deleting the file.");
+		}
+	}
+
+}
